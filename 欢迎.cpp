07c1d4978d743367dd1e06982a4500ea737af5d6
RicardoY808 <72294960+RicardoY808@users.noreply.github.com>
2020-10-10 09:51:36 +0800

    Add files via upload

---
+++ 欢迎.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{cout<<"************************************"<<endl;  
+cout<< "*  欢迎使用学生成绩管理系统1.0"  <<endl;
+cout<<"*    杨积鑫   计算机2003班 2020.10.8"<<endl;
+cout<<"*    1,输入语，数，英三门课程成绩"<<endl;
+cout<<"*    2,查询程序     "<<endl;
+cout<<"*   3,求平均分    "<<endl;
+cout<<"*   4,退出    "<<endl;
+cout<<"*  请输入（1-4）选择： "<<endl;
+cout<<"* "<<endl;
+cout<<"**************************************"<<endl;
+return 0;
+}
